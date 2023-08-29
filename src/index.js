8ca1d336df669295711ebfe46ce954e0cb94c89a
Jigar-gokani <jigar.gokani@somaiya.edu>
2023-08-29 11:59:34 +0530

    hotfix deploy

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+// import { Toast } from "react-toastify/dist/components";
+import App from "./App";
+import "./index.css";
+import { store } from "./redux/Store"
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+      <Provider store={store}>
+        <App/>
+        <Toaster/>
+     </Provider>
+  </BrowserRouter>
+  
+    
+
+
+
+  
+);
